131b01e41e617f013315a9b7d6592d4b39337a61
0xKeyser <69942722+0xKeyser@users.noreply.github.com>
2020-09-09 13:54:00 +0700

    Update DYOR.sol

--- contracts/eip20/DYOR.sol
+++ contracts/eip20/DYOR.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.4.21;
 import "./EIP20Interface.sol";
 
 
-contract YLOL is EIP20Interface {
+contract XXX is EIP20Interface {
 
     uint256 constant private MAX_UINT256 = 2**256 - 1;
     mapping (address => uint256) public balances;
@@ -18,7 +18,7 @@ contract YLOL is EIP20Interface {
     uint8 public decimals;                
     string public symbol;                 
 
-    function YLOL(
+    function XXX(
         uint256 _initialAmount,
         string _tokenName,
         uint8 _decimalUnits,
